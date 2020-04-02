d55d2a8a63147116904dad63cac34fcce6aaf17a
yangbob <yangsangho22@gmail.com>
2020-04-02 16:54:23 +0900

    build release version : 1.0.2(3), set code optimization and proguard, change assert library(from Truth to Hamcrest)

--- app/src/test/java/kr/yangbob/memorization/data/SimpleDateTest.kt
+++ app/src/test/java/kr/yangbob/memorization/data/SimpleDateTest.kt
@@ -1,6 +1,8 @@
 package kr.yangbob.memorization.data
 
-import com.google.common.truth.Truth.assertThat
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.*
 import org.junit.Test
 import java.text.DateFormat
 import java.util.*
@@ -9,111 +11,92 @@ class SimpleDateTest {
     private val pastDate = SimpleDate.newInstanceFromDateInt(20200131)
     private val todayDate = SimpleDate.newInstanceToday()
     private val futureDate = SimpleDate.newInstanceFromDateInt(20250528)
+
     // 테스트 실행 전 아래 변수 값 변경하기
-    private val todayDateInt = 20200330
+    private val todayDateInt = 20200402
 
     @Test
-    fun newInstanceTest(){
-        assertThat(pastDate).isNotNull()
-        assertThat(todayDate).isNotNull()
-        assertThat(futureDate).isNotNull()
+    fun newInstanceTest() {
+        assertThat(pastDate, notNullValue())
+        assertThat(todayDate, notNullValue())
+        assertThat(futureDate, notNullValue())
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun newInstanceFromDateIntExceptionTest() {
+        var simpleDate = SimpleDate.newInstanceFromDateInt(222222)
+        simpleDate = SimpleDate.newInstanceFromDateInt(22220528)
+        simpleDate = SimpleDate.newInstanceFromDateInt(20201328)
+        simpleDate = SimpleDate.newInstanceFromDateInt(20201200)
     }
 
     @Test
-    fun newInstanceFromDateIntExceptionTest(){
-        try{
-            val simpleDate = SimpleDate.newInstanceFromDateInt(222222)
-        } catch (e: IllegalArgumentException){
-            println("newInstanceFromDateIntExceptionTest pass : check length exception")
-        }
-
-        try{
-            val simpleDate = SimpleDate.newInstanceFromDateInt(22220528)
-        } catch (e: IllegalArgumentException){
-            println("newInstanceFromDateIntExceptionTest pass : check year exception")
-        }
-
-        try{
-            val simpleDate = SimpleDate.newInstanceFromDateInt(20201328)
-        } catch (e: IllegalArgumentException){
-            println("newInstanceFromDateIntExceptionTest pass : check year exception")
-        }
-
-        try{
-            val simpleDate = SimpleDate.newInstanceFromDateInt(20201200)
-        } catch (e: IllegalArgumentException){
-            println("pass : check year exception")
-        }
-    }
-
-    @Test
-    fun equalsAndCompareTest(){
+    fun equalsAndCompareTest() {
         val newTodayDate = SimpleDate.newInstanceFromDateInt(todayDateInt)
-        assertThat(newTodayDate.hashCode()).isEqualTo(todayDate.hashCode())
-        assertThat(newTodayDate).isEquivalentAccordingToCompareTo(todayDate)
-        assertThat(newTodayDate).isEqualTo(todayDate)
-        assertThat(newTodayDate).isNotEqualTo(futureDate)
-        assertThat(newTodayDate).isLessThan(futureDate)
-        assertThat(newTodayDate).isGreaterThan(pastDate)
+        assertThat(newTodayDate.hashCode(), `is`(todayDate.hashCode()))
+        assertThat(newTodayDate, `is`(todayDate))
+        assertThat(newTodayDate, not(equalTo(futureDate)))
+        assertThat(newTodayDate, lessThan(futureDate))
+        assertThat(newTodayDate, greaterThan(pastDate))
     }
 
     @Test
-    fun cloneTest(){
+    fun cloneTest() {
         val cloneDate = todayDate.clone()
-        assertThat(cloneDate).isEqualTo(todayDate)
+        assertThat(cloneDate, `is`(todayDate))
     }
 
     @Test
-    fun getFormattedDateTest(){
+    fun getFormattedDateTest() {
         val defaultFormattedDate = futureDate.getFormattedDate()
         val fullFormattedDate = futureDate.getFormattedDate(DateFormat.FULL)
-        assertThat(defaultFormattedDate).isEqualTo("2025. 5. 28")
-        assertThat(fullFormattedDate).isEqualTo("2025년 5월 28일 수요일")
+        assertThat(defaultFormattedDate, `is`("2025. 5. 28"))
+        assertThat(fullFormattedDate, `is`("2025년 5월 28일 수요일"))
     }
 
     @Test
-    fun getMethodTest(){
-        assertThat(futureDate.getDateInt()).isEqualTo(20250528)
-        assertThat(futureDate.getYear()).isEqualTo(2025)
-        assertThat(futureDate.getMonth()).isEqualTo(5)
-        assertThat(futureDate.getDay()).isEqualTo(28)
+    fun getMethodTest() {
+        assertThat(futureDate.getDateInt(), `is`(20250528))
+        assertThat(futureDate.getYear(), `is`(2025))
+        assertThat(futureDate.getMonth(), `is`(5))
+        assertThat(futureDate.getDay(), `is`(28))
     }
 
     @Test
-    fun setDateTest(){
+    fun setDateTest() {
         val cloneDate = todayDate.clone()
 
         cloneDate.setDate(Calendar.YEAR, 2025)
-        assertThat(cloneDate.getYear()).isEqualTo(futureDate.getYear())
+        assertThat(cloneDate.getYear(), `is`(futureDate.getYear()))
 
         cloneDate.setDate(Calendar.MONTH, 5)
-        assertThat(cloneDate.getMonth()).isEqualTo(futureDate.getMonth())
+        assertThat(cloneDate.getMonth(), `is`(futureDate.getMonth()))
 
         cloneDate.setDate(Calendar.DAY_OF_MONTH, 28)
-        assertThat(cloneDate.getDay()).isEqualTo(futureDate.getDay())
+        assertThat(cloneDate.getDay(), `is`(futureDate.getDay()))
 
-        assertThat(cloneDate).isEqualTo(futureDate)
+        assertThat(cloneDate, `is`(futureDate))
     }
 
     @Test
-    fun addDateTest(){
+    fun addDateTest() {
         val cloneDate = futureDate.clone()
 
         cloneDate.addDate(Calendar.YEAR, 4)
-        assertThat(cloneDate.getYear()).isEqualTo(2029)
+        assertThat(cloneDate.getYear(), `is`(2029))
         cloneDate.addDate(Calendar.YEAR, -4)
-        assertThat(cloneDate.getYear()).isEqualTo(2025)
+        assertThat(cloneDate.getYear(), `is`(2025))
 
         cloneDate.addDate(Calendar.MONTH, 3)
-        assertThat(cloneDate.getMonth()).isEqualTo(8)
+        assertThat(cloneDate.getMonth(), `is`(8))
         cloneDate.addDate(Calendar.MONTH, -3)
-        assertThat(cloneDate.getMonth()).isEqualTo(5)
+        assertThat(cloneDate.getMonth(), `is`(5))
 
         cloneDate.addDate(Calendar.DAY_OF_MONTH, 3)
-        assertThat(cloneDate.getDay()).isEqualTo(31)
+        assertThat(cloneDate.getDay(), `is`(31))
         cloneDate.addDate(Calendar.DAY_OF_MONTH, -3)
-        assertThat(cloneDate.getDay()).isEqualTo(28)
+        assertThat(cloneDate.getDay(), `is`(28))
 
-        assertThat(cloneDate).isEqualTo(futureDate)
+        assertThat(cloneDate, `is`(futureDate))
     }
 }
